c0f609dfc20bb68717bb8a07fd073cad262ad90b
CSoft <del.afalina@gmail.com>
2022-03-10 13:09:00 +0300

    work with fluent +04 work

--- tdm_04/src/test/kotlin/ru/cs/tdm/cases/WaitTest.kt
+++ tdm_04/src/test/kotlin/ru/cs/tdm/cases/WaitTest.kt
@@ -5,24 +5,25 @@ import io.github.bonigarcia.wdm.WebDriverManager
 import org.junit.jupiter.api.*
 import org.openqa.selenium.chrome.ChromeDriver
 import org.junit.jupiter.api.Assertions.*
+import org.openqa.selenium.support.ui.FluentWait
+import org.openqa.selenium.support.ui.WebDriverWait
 import ru.cs.tdm.code.Login
 import ru.cs.tdm.code.Tools
 import ru.cs.tdm.data.ConfProperties
+import ru.cs.tdm.pages.ToolsPage
+import java.time.Duration
+
 
 /**
- * при выходе на http://tdms-srv2a:444/client/#objects/ открывает страницу аутентификации;
- * Пользователь производит ввод валидных логина и пароля;
  *
- * Пользователь осуществляет выход из аккаунта путем нажатия на имя пользователя в верхнем правом углу окна с последующим нажатием на кнопку «Выйти…».
- *
- * Тест считается успешно пройденным в случае, когда пользователю удалось выполнить все вышеперечисленные пункты.
  */
-@DisplayName("Testing Tools Menu-Icons Test")
-class HeadTest {
+@DisplayName("Testing Wait Test")
+class WaitTest {
     // переменная для драйвера
     private lateinit var driver: WebDriver
     // объявления переменных на созданные ранее классы-страницы
-    private lateinit var tools: Tools
+    lateinit var toolsPage: ToolsPage
+    lateinit var tools: Tools
 
     /**
      * осуществление первоначальной настройки
@@ -40,15 +41,19 @@ class HeadTest {
 
         // Создаем экземпляры классов созданных ранее страниц, и присвоим ссылки на них.
         // В качестве параметра указываем созданный перед этим объект driver,
+        toolsPage = ToolsPage(driver)
         tools = Tools(driver)
         //окно разворачивается на полный экран
         driver.manage().window().maximize()
-
+        // задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10))  // Неявное ожидание
+        val waitDriver = WebDriverWait(driver, Duration.ofSeconds(10))     // Явное ожидание
+        val waitFluen = FluentWait<WebDriver>(driver)                             // Беглое ожидание
         val loginpageTDM = ConfProperties.getProperty("loginpageTDM")
         driver.get(loginpageTDM)
-
         val loginTDM = ConfProperties.getProperty("loginTDM")
         val passwordTDM = ConfProperties.getProperty("passwordTDM")
+
         Login(driver).loginIn(loginTDM, passwordTDM)
     }
 
@@ -57,88 +62,167 @@ class HeadTest {
      *
      */
 
-    @RepeatedTest(50)
+    @RepeatedTest(3)
     @DisplayName("Testing each menu separately")
     fun headMenuTest()  {
+        val sleepSec = 500L
 
         tools.qtipClickLast("Главное меню")
-        assertTrue(tools.titleContain("TDM365"))
+        Thread.sleep(sleepSec)
+        assertEquals("TDM365", driver.title.split(" ").toTypedArray()[0])
         assertTrue(tools.qtipLast("Объекты")?.getAttribute("aria-pressed") =="true")
 
         tools.qtipClickLast("Рабочий стол")
-        assertTrue(tools.titleContain("Рабочий"))
+        Thread.sleep(sleepSec)
+        assertEquals("Рабочий", driver.title.split(" ").toTypedArray()[0])
         assertTrue(tools.qtipLast("Рабочий стол")?.getAttribute("aria-pressed") =="true")
 
         tools.qtipClickLast("Объекты")
-        assertTrue(tools.titleContain("TDM365"))
+        Thread.sleep(sleepSec)
+        assertEquals("TDM365", driver.title.split(" ").toTypedArray()[0])
         assertTrue(tools.qtipLast("Объекты")?.getAttribute("aria-pressed") =="true")
 
         tools.qtipClickLast("Почта")
-        assertTrue(tools.titleContain("Почта"))
+        Thread.sleep(sleepSec)
+        assertEquals("Почта", driver.title.split(" ").toTypedArray()[0])
         assertTrue(tools.qtipLast("Почта")?.getAttribute("aria-pressed") =="true")
 
         tools.qtipClickLast("Совещания")
-        assertTrue(tools.titleContain("Совещания"))
+        Thread.sleep(sleepSec)
+        assertEquals("Совещания", driver.title.split(" ").toTypedArray()[0])
         assertTrue(tools.qtipLast("Совещания")?.getAttribute("aria-pressed") =="true")
 
         tools.qtipClickLast("Справка")
-        //assertTrue(tools.titleContain("Справка").not())      // Исправить!!! NOT
+        Thread.sleep(sleepSec)
+       // assertEquals("Справка", driver.title.split(" ").toTypedArray()[0])
         assertTrue(tools.qtipLast("Справка")?.getAttribute("aria-pressed") =="true")
 
-        //input [@data-qtip = "Введите текст"] //input [contains(@data-qtip , "Введите текст")]
         tools.qtipClickLast("Объекты")
+        Thread.sleep(sleepSec)
+        // @FindBy(xpath = "//input[contains(@placeholder,'Найти')]")
+        tools.qtipClickLast("Справка")
+        Thread.sleep(sleepSec)
+        // assertEquals("Справка", driver.title.split(" ").toTypedArray()[0])
+        assertTrue(tools.qtipLast("Справка")?.getAttribute("aria-pressed") =="true")
+
+        // //input [@data-qtip = "Введите текст"] //input [contains(@data-qtip , "Введите текст")]
+        tools.qtipClickLast("Объекты")
+        Thread.sleep(sleepSec)
         tools.qtipLast("Введите текст")?.sendKeys("Лебедев")
         tools.qtipClickLast("Искать")
-        assertTrue(tools.titleContain("Результаты"))
+        Thread.sleep(sleepSec)
+        assertEquals("Результаты", driver.title.split(" ").toTypedArray()[0])
+        //assertTrue(tools.qtipLast("Введите текст")?.getAttribute("aria-pressed") =="true")  // Здесь не работает
+        //println ( "Строка поиска = ${ tools.qtipLast("Введите текст")?.text} ")  // Пусто
         tools.qtipClickLast("Очистить")
-        tools.qtipClickLast("Объекты")
 
+        /**
+         * <div id="window-1241_header-title-textEl" data-ref="textEl"
+         * class="x-title-text x-title-text-default x-title-item"
+         * unselectable="on" role="presentation"
+         * >Окно сообщений</div>
+         * <div id="window-1241-ghost_header-title-textEl" data-ref="textEl"
+         * class="x-title-text x-title-text-default x-title-item"
+         * unselectable="on" role="presentation"
+         * >Окно сообщений</div>
+         * //div[starts-with(@id, "window") and contains(@id, "title") and contains(@id, "textEl") and not(contains(@id, "ghost"))]
+         */
         tools.qtipClickLast("Уведомления")
-        assertEquals("Окно сообщений", tools.windowTitle())
+        Thread.sleep(sleepSec)
+        val windowNext = tools.xpathLast("//div[starts-with(@id, 'window') and contains(@id, 'title') and contains(@id, 'textEl') and not(contains(@id, 'ghost'))]")?.text?:""
+        assertEquals("Окно сообщений", windowNext)
         tools.closeXLast()
     }
-    @RepeatedTest(25)
+    @RepeatedTest(3)
     @DisplayName("Testing Tools Box")
-    fun toolsTest()  {
+    fun toolsTestTDM()  {
+        val sleepSec = 3000L
+    
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Главное меню")
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Показать/скрыть дерево")
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Показать/скрыть дерево")
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Показать/скрыть панель предварительного просмотра")
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Показать/скрыть панель предварительного просмотра")
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Создать фильтр")
+        Thread.sleep(sleepSec)
         tools.closeXLast()
-        tools.qtipClickLast("Обновить")
+
+        Thread.sleep(sleepSec)
+        toolsPage.toolBtn("Обновить")?.click()
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Администрирование групп")
+        Thread.sleep(sleepSec)
         tools.closeXLast()
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Настройка Camunda")
+        Thread.sleep(sleepSec)
         tools.closeXLast()
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Удалить структуру объектов")
+        Thread.sleep(sleepSec)
         tools.closeXLast()
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Создать объект разработки")
+        Thread.sleep(sleepSec)
         tools.closeXLast()
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Параметры системы")
+        Thread.sleep(sleepSec)
         tools.closeXLast()
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Поток - Проверка статуса проекта")
         repeat(3) {
+            Thread.sleep(sleepSec)
             tools.closeXLast()
         }
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Поток 0 - Отправка проекта")
+        Thread.sleep(sleepSec)
         tools.closeXLast()
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Поток 1 - Отправка передаточного документа")
         repeat(2) {
+            Thread.sleep(sleepSec)
             tools.closeXLast()
         }
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Поток 2.1 - Ответ о результате передаче РЗ")
         repeat(3) {
+            Thread.sleep(sleepSec)
             tools.closeXLast()
         }
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Поток 3 - Отправка ответов на замечания")
         repeat(2) {
+            Thread.sleep(sleepSec)
             tools.closeXLast()
         }
+
+        Thread.sleep(sleepSec)
         tools.qtipClickLast("Настройка шаблона уведомлений")
+        Thread.sleep(sleepSec)
         tools.closeXLast()
         repeat(3) {
+            Thread.sleep(sleepSec)
         }
     }
 
