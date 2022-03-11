acc187dfc979c5e984ba49c2f7c18d09c9ab0656
CSoft <del.afalina@gmail.com>
2022-03-11 20:41:36 +0300

    work with fluent +05 work quick

--- tdm_05/src/test/kotlin/ru/cs/tdm/cases/LoginTestTDM.kt
+++ tdm_05/src/test/kotlin/ru/cs/tdm/cases/LoginTestTDM.kt
@@ -1,16 +1,15 @@
 package ru.cs.tdm.cases
 
 import ru.cs.tdm.data.ConfProperties
+import ru.cs.tdm.pages.LoginPage
+import ru.cs.tdm.pages.MainViewHeaderPage
 import org.openqa.selenium.WebDriver
 import org.junit.jupiter.api.BeforeEach
 import io.github.bonigarcia.wdm.WebDriverManager
 import org.openqa.selenium.chrome.ChromeDriver
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.RepeatedTest
-import org.openqa.selenium.chrome.ChromeOptions
-import ru.cs.tdm.code.Login
+import org.junit.jupiter.api.Test
 import java.time.Duration
 
 /**
@@ -21,10 +20,12 @@ import java.time.Duration
  *
  * Тест считается успешно пройденным в случае, когда пользователю удалось выполнить все вышеперечисленные пункты.
  */
-class LoginTest {
+class LoginTestTDM {
     // переменная для драйвера
     private lateinit var driver: WebDriver
-    private lateinit var login: Login
+    // объявления переменных на созданные ранее классы-страницы
+    lateinit var loginPage: LoginPage
+    lateinit var mainViewHeaderPage: MainViewHeaderPage
 
     /**
      * осуществление первоначальной настройки
@@ -33,41 +34,58 @@ class LoginTest {
     fun setup() {
         // создание экземпляра драйвера (т.к. он объявлен в качестве переменной):
         WebDriverManager.chromedriver().setup()
-        driver = ChromeDriver(ChromeOptions().addArguments("--window-position=2000,0"))
+        driver = ChromeDriver()
+
+        // Создаем экземпляры классов созданных ранее страниц, и присвоим ссылки на них.
+        // В качестве параметра указываем созданный перед этим объект driver,
+        loginPage = LoginPage(driver)
+        mainViewHeaderPage = MainViewHeaderPage(driver)
         //окно разворачивается на полный экран
         driver.manage().window().maximize()
         // задержка на выполнение теста = 10 сек.
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10))
         //получение ссылки на страницу входа из файла настроек
-        val loginpageTDM = ConfProperties.getProperty("loginpageTDM")
-        driver.get(loginpageTDM)
-        login = Login(driver)
+        driver.get(ConfProperties.getProperty("loginpageTDM"))
     }
 
     /**
      * тестовый метод для осуществления аутентификации
      */
-    @RepeatedTest(30)
-    @DisplayName("Проверка входа-выхода Login - Password")
+    @Test
     fun loginTest() {
-
+        //получение доступа к методам класса LoginPage для взаимодействия с элементами страницы
         //значение login/password берутся из файла настроек по аналогии с chromedriver и loginpage
-        val loginTDM = ConfProperties.getProperty("loginTDM")
-        val passwordTDM = ConfProperties.getProperty("passwordTDM")
-
-        login.loginIn(loginTDM, passwordTDM)
+        //вводим логин  ХАЛТУРА - только тема 1
+        loginPage.inputLogin(ConfProperties.getProperty("loginTDM"))
+        //вводим пароль
+        loginPage.inputPasswd(ConfProperties.getProperty("passwordTDM"))
+        //нажимаем кнопку входа
+        loginPage.clickLoginBtn()
 
         //получаем отображаемый логин
-        //val user = login.loginUserName()
+        Thread.sleep(3000)
+        val user = mainViewHeaderPage.firstUserName // Халтура - button
         //и сравниваем его с логином из файла настроек
-        //assertEquals(loginTDM, user)
-        //assertTrue(login.loginUserNameWait(loginTDM))
-        assertTrue(login.qtipLoginUserNameWait(loginTDM))
+        assertEquals(ConfProperties.getProperty("loginTDM"), user)
     }
 
     @AfterEach
         fun tearDown() {
-            login.loginOut()
+            mainViewHeaderPage.ClickTDMSWeb()
+            mainViewHeaderPage.ClickDesktop()
+            mainViewHeaderPage.ClickObjects()
+            mainViewHeaderPage.ClickMail()
+            mainViewHeaderPage.ClickChat()
+            mainViewHeaderPage.ClickHelp()
+            mainViewHeaderPage.ClickObjects()
+            mainViewHeaderPage.InputSearch("Лебедев")
+            mainViewHeaderPage.ClickSearchEnter() // КОСТЫЛЬ посылаю Enter вместо Лупы
+            // mainViewHeaderPage.ClickMagnifier(); // не работает
+            mainViewHeaderPage.ClickMessages()
+            mainViewHeaderPage.CloseMessages() // КОСТЫЛЬ посылаю ESC вместо крестика закрытия окна
+            mainViewHeaderPage.entryMenu() // Халтура - button
+            mainViewHeaderPage.userLogout()
+            mainViewHeaderPage.logoutOKBtn()
             driver.quit() //  закрытия окна браузера
         }
 }
