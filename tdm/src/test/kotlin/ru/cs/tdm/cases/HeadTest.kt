e7c8c091cfac5deacfff706f14384df4d1085bca
CSoft <del.afalina@gmail.com>
2022-03-18 03:44:03 +0300

    work with fluent +06 work quick 1.2.9

--- tdm/src/test/kotlin/ru/cs/tdm/cases/HeadTest.kt
+++ tdm/src/test/kotlin/ru/cs/tdm/cases/HeadTest.kt
@@ -56,7 +56,7 @@ class HeadTest {
      *
      */
 
-    @RepeatedTest(50)
+    @RepeatedTest(25)
     @DisplayName("Testing each menu separately")
     fun headMenuTest()  {
 
@@ -102,7 +102,7 @@ class HeadTest {
         assertEquals("Окно сообщений", tools.windowTitle())
         tools.closeXLast()
     }
-    @RepeatedTest(25)
+    @RepeatedTest(15)
     @DisplayName("Testing Tools Box")
     fun toolsTest()  {
         tools.qtipClickLast("Главное меню")
