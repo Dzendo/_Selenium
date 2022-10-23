dd3a44b0f467d1cc1838ae5b9efd81d0d6122bb7
CSoft <del.afalina@gmail.com>
2022-10-23 13:24:33 +0300

    1.2.31_6.1.172 + tdm_pass update

---
+++ tdm_pass/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Hello TDM365 example 3")
+    println("Hello World!")
+    println("Hello World!")
+    println("TDM 365 ")
+    println( "window-1156_header-targetEl".split('-','_'))
+    val aaa = "window-1156_header-targetEl".split('-','_')
+    println( aaa[2])
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
--- tdm_pass/src/main/kotlin/Root.kt
+++ tdm_pass/src/main/kotlin/Root.kt
@@ -1,7 +1,20 @@
+import ru.cs.tdm.cases.Pass
+
 fun main(args: Array<String>) {
     println("Hello World! 2")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    val pass = Pass()
+
+    repeat(5) {
+        println("========================================== ${it+1} ======================================= ")
+        Pass.beforeAll()
+    pass.n01_CreateUserPass()
+    pass.n02_enterUserPass()
+    pass.n03_changeUserPass()
+    pass.n09_deleteUserPass()
+        Pass.afterAll()
+    }
 }
