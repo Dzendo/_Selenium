93e088ed0632e32d929f027103dcb653ae305193
Dzendo <41204679+Dzendo@users.noreply.github.com>
2023-07-14 20:48:01 +0300

    Новый tdm = react+sench Selenium_2 + Selenide_2

--- tdm-selenide/src/main/kotlin/ru/cs/tdm/data/TestsProperties.kt
+++ tdm-selenide/src/main/kotlin/ru/cs/tdm/data/TestsProperties.kt
@@ -4,7 +4,7 @@ import ru.cs.tdm.ui.StartDialog
 const val Tdms = "Tdms"
 const val TDMS = "TDMS"
 const val TDM365 = "TDM365"
-const val VER = "1.4.48_7.0.35 ALL_2"
+const val VER = "1.4.48_7.0.35 Selenide_2"
 object TestsProperties {
 
     var fluentInDurationNomber = 13000L
